80a19c0ff6750f956d744e3e326cbd352cc2b24d
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-04 00:50:51 +0300

    component search form and git added, search button customized successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
